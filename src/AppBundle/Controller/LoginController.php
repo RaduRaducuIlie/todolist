d6c62e7dcfe1e061296704f3c9be6ceac5493c63
Radu <radu_ilie_92@yahoo.com>
2016-11-19 21:23:37 +0200

    Added registration.

---
+++ src/AppBundle/Controller/LoginController.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace AppBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Description of LoginController
+ *
+ * @author Radu
+ */
+class LoginController {
+    //put your code here
+     public function loginAction(Request $request){
+         $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContextInterface::AUTHENTICATION_ERROR
+            );
+        } else {
+            $error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
+        }
+
+        return $this->render(
+            'todo/login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $session->get(SecurityContextInterface::LAST_USERNAME),
+                'error'         => $error,
+            )
+        );
+         
+     }
+}
---
+++ src/AppBundle/Repository/UserRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace AppBundle\Repository;
+
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
+use Doctrine\ORM\EntityRepository;
+
+class UserRepository extends EntityRepository implements UserLoaderInterface
+{
+    public function loadUserByUsername($username)
+    {
+        return $this->createQueryBuilder('u')
+            ->where('u.username = :username OR u.email = :email')
+            ->setParameter('username', $username)
+            ->setParameter('email', $username)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+}
